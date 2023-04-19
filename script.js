ea4e1f9cdb954b6f797dcbe9baba83a9d883758e
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-20 04:10:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ size();
 function size(){
     const width = window.innerWidth;
     const height =window.innerHeight;
-    h.innerText=`Width : ${width} and Height: ${height}`;
+    h.innerText=`Width: ${width} and Height: ${height}`;
 }
 
 window.addEventListener("resize",size);
