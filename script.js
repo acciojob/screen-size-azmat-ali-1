4a1ed4f7e53cff04caa0146da77fb7a0420142e2
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-20 03:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const h = document.getElementById("sizeInfo");
+size();
+
+function size(){
+    const width = window.innerWidth;
+    const height =window.innerHeight;
+    h.children[0].textContent=`Width : ${width} and Height: ${height}`;
+}
+
+window.addEventListener("resize",size);
